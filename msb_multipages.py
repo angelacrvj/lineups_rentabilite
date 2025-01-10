9a8357ad4bc90d6afe2692135e36e2a191f8ac40
Angela <angela.correia@outlook.fr>
2025-01-10 11:17:58 +0100

    maj

--- msb_multipages.py
+++ msb_multipages.py
@@ -8,7 +8,7 @@ import os
 
 # Chargement des données
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, "lineups_rentabilite.csv")
+file_path = os.path.join(script_dir, "lineups_rentabilite_wt_league.csv")
 data = pd.read_csv(file_path)
 
 # Stats 
