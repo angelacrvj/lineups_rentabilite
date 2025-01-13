11f208acde835fab609603704459e56316888ae6
Angela <angela.correia@outlook.fr>
2025-01-13 21:24:07 +0100

    maj

--- msb_multipages.py
+++ msb_multipages.py
@@ -251,12 +251,12 @@ def page_analyse_rentabilite():
         opponent_data_filtered = opponent_data  # on prévoit les cas où monsieur basket ne filtre pas les joueurs 
 
     # Dimensionnement de la heatmap si "Ligue" sélectionné 
-    if opponent_name == "Ligue":
+    #if opponent_name == "Ligue":
         # Taille personnalisée pour "Ligue"
-        fig, ax = plt.subplots(figsize=(10, 2))  # Ajuste selon la taille souhaitée pour "Ligue"
-    else:
+    #    fig, ax = plt.subplots(figsize=(10, 2))  # Ajuste selon la taille souhaitée pour "Ligue"
+    #else:
         # Pas de figsize spécifié ici, matplotlib ajustera automatiquement la taille
-        fig, ax = plt.subplots()
+    #    fig, ax = plt.subplots()
 
 
     # Affichage Heatmap : Équipe de référence vs Équipe adverse
@@ -267,7 +267,7 @@ def page_analyse_rentabilite():
             st.image(team_logos[team_name], width=50)
     with col2:
         matchup_df = calculate_matchup(team_data_filtered, opponent_data_filtered)
-        plot_heatmap(matchup_df, f"Heatmap pour {team_name} contre {opponent_name}", ax)
+        plot_heatmap(matchup_df, f"Heatmap pour {team_name} contre {opponent_name}", plt.gca())
 
     # Affichage Heatmap : Équipe adverse vs Équipe de référence
     st.subheader(f"Heatmap : {opponent_name} vs {team_name}")
