3ca95bee538ebb65d3bd1d46ffb272da951b6567
Angela <angela.correia@outlook.fr>
2025-01-13 16:20:52 +0100

    maj

--- msb_lineup_rentabilite.py
+++ msb_lineup_rentabilite.py
@@ -7,7 +7,7 @@ import random
 import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(script_dir, "lineups_rentabilite.csv")
+file_path = os.path.join(script_dir, "lineups_rentabilite_1301.csv")
 data = pd.read_csv(file_path)
 
 # Stats 
